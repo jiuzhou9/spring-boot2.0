fe8548dd23a11a7f73274fcd47b206bde67f9085
Wang Jiuzhou <jiuzhou@shanshu.ai>
2019-06-13 16:05:16 +0800

    理解spring Application

--- spring-boot-spring-application/src/test/java/cn/jiuzhou/SpringApplicationBootStrapTest.java
+++ spring-boot-spring-application/src/test/java/cn/jiuzhou/SpringApplicationBootStrapTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple SpringApplicationBootStrap.
  */
-public class AppTest 
+public class SpringApplicationBootStrapTest
 {
     /**
      * Rigorous Test :-)
